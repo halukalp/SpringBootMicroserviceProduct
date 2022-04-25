c681d0bcea034fe7ca65580ae50b19d5ef385f01
halukalp <haluk.allp@gmail.com>
2022-04-25 13:06:22 +0300

    initial commit

---
+++ src/test/java/com/akis_yazilim/oracle/melih/product/SpringBootMicroserviceProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akis_yazilim.oracle.melih.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMicroserviceProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
