f1df4919957a4c7dc93035a0d0af5a516064418b
halukalp <haluk.allp@gmail.com>
2022-04-26 16:07:39 +0300

    initial commit

---
+++ src/main/java/com/akis_yazilim/oracle/melih/product/controller/ProductController.java
@@ -0,0 +1,43 @@
+package com.akis_yazilim.oracle.melih.product.controller;
+
+import com.akis_yazilim.oracle.melih.product.model.entity.Product;
+import com.akis_yazilim.oracle.melih.product.model.service.AbstractProductService;
+import com.akis_yazilim.oracle.melih.product.model.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequestMapping("api/product")
+@RestController
+public class ProductController
+{
+    @Autowired
+    private AbstractProductService productService;
+
+    @PostMapping
+    public ResponseEntity<Product> save(@RequestBody Product product)
+    {
+        Product savedProduct = productService.save(product);
+
+        return new ResponseEntity<>(savedProduct , HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("{productID}")
+    public ResponseEntity delete(@PathVariable Integer productID)
+    {
+        productService.deleteByID(productID);
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Product>> getAll()
+    {
+        List<Product> productList = productService.findAll();
+
+        return ResponseEntity.ok(productList);
+    }
+}
--- src/main/java/com/akis_yazilim/oracle/melih/product/model/service/ProductService.java
+++ src/main/java/com/akis_yazilim/oracle/melih/product/model/service/ProductService.java
@@ -2,38 +2,59 @@ package com.akis_yazilim.oracle.melih.product.model.service;
 
 import com.akis_yazilim.oracle.melih.product.model.entity.Product;
 import com.akis_yazilim.oracle.melih.product.utility.ExceptionMessageType;
+import com.akis_yazilim.oracle.melih.product.utility.Util;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
 public class ProductService extends AbstractProductService
 {
-
     @Override
     protected List<Product> findByPriceGreaterThan(Double limit)
     {
-        return null;
+        return productRepository.findByPriceGreaterThan(limit);
     }
 
     @Override
-    protected List<Product> findByPriceGreaterThanEqual(Double limit) {
-        return null;
+    protected List<Product> findByPriceGreaterThanEqual(Double limit)
+    {
+        return productRepository.findByPriceGreaterThanEqual(limit);
     }
 
     @Override
-    public void deleteByID(Integer id) {
-
+    public void deleteByID(Integer id)
+    {
+        try
+        {
+            productRepository.deleteById(id);
+        }
+        catch (IllegalArgumentException e)
+        {
+            Util.showExceptionInfo(e);
+        }
     }
 
     @Override
-    public Product save(Product entity) {
-        return null;
+    public Product save(Product entity)
+    {
+        try
+        {
+            entity.setCreated(new Date());
+            return productRepository.save(entity);
+        }
+        catch (IllegalArgumentException e)
+        {
+            Util.showExceptionInfo(e);
+            return null;
+        }
     }
 
     @Override
-    public List<Product> findAll() {
-        return null;
+    public List<Product> findAll()
+    {
+        return productRepository.findAll();
     }
 
     @Override
